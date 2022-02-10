e4effd13e6cf4d10a76351114107cac7772f03ce
Alex <alexander.suer@outlook.com>
2022-02-10 12:17:32 -0500

    Updated to Daniel's v5 version and added dynamic directory

--- Scripts/BaseCode.m
+++ Scripts/BaseCode.m
@@ -1,31 +1,92 @@
 clc;
 clear;
 close all;
-directoryF='\\clusterfsnew.ceas1.uc.edu\students\perlitda\My Documents\Hw2\Training\Training\Faulty\';
-HorF=0;
-structure=struct([]);
-structure = createstructure(directoryF,structure,HorF);
 
-directoryH='\\clusterfsnew.ceas1.uc.edu\students\perlitda\My Documents\Hw2\Training\Training\Healthy\';
-HorF=1;
-structure = createstructure(directoryH,structure,HorF);
+% Find Project Directory
+% Assumes script is in a 'scripts' folder below the top project directory
+if ispc
+    delimeter = "\";
+else
+    delimeter = "/";
+end
+scriptPath = split(mfilename('fullpath'),delimeter);
+projectPath = join(scriptPath(1:end-2,:),delimeter);
+projectPath = projectPath{1};
 
-directorys='\\clusterfsnew.ceas1.uc.edu\students\perlitda\My Documents\Hw2\results\';
+directoryF=join([projectPath, "Training","Faulty",""],delimeter);
+structuretrain=struct([]);
+structuretrain = createstructure(directoryF,structuretrain,0);
 
-for i = 1:length(structure)
-    [pathstr,name,ext] = fileparts(structure(i).name);
-    figure(i)
-    subplot(2,1,1)
-    plot(structure(i).time,structure(i).acc)
-    title(name)
-    xlabel('Time')
-    ylabel('Acc')
-    subplot(2,1,2)
-    plot(structure(i).freq,structure(i).mag)
-    xlabel('freq(Hz)')
-    xlim([0 100])
-    ylabel('Mag')
-    saveas(gcf,strcat(directorys,name,'.pdf'))
+directoryH=join([projectPath, "Training","Healthy",""],delimeter);
+structuretrain = createstructure(directoryH,structuretrain,1);
+
+structuretest=struct([]);
+directoryTe=join([projectPath,"Testing",""],delimeter);
+structuretest = createstructure(directoryTe,structuretest,-1);
+
+directorys=join([projectPath,"results",""],delimeter);
+%plotcurves(structuretrain, directorys)
+%plotcurves(structuretest, directorys)
+
+FMTrain=[[structuretrain.magnear20]',[structuretrain.stdv20]',[structuretrain.magnear40]',[structuretrain.stdv40]'];
+NamesTrain=createnamevec(structuretrain);
+HorFVTrain=[structuretrain.HorF]';
+
+
+FMTest=[[structuretest.magnear20]',[structuretest.magnear40]',[structuretest.stdv20]',[structuretest.stdv40]'];
+NamesTest=createnamevec(structuretest);
+
+% figure(1)
+% plot([structuretrain.magnear20]')
+% xlabel('File')
+% ylabel('FFT Mag Near First Peak')
+% saveas(gcf,strcat(directorys,'FFT Mag Near First Peak','.pdf'))
+% figure(2)
+% plot([structuretrain.magnear40]')
+% xlabel('File')
+% ylabel('FFT Mag Near Sec Peak')
+% saveas(gcf,strcat(directorys,'FFT Mag Near Sec Peak','.pdf'))
+% figure(3)
+% plot([structuretrain.stdv20]')
+% xlabel('File')
+% ylabel('Stdev Near First Peak')
+% saveas(gcf,strcat(directorys,'Stdev Near First Peak','.pdf'))
+% figure(4)
+% plot([structuretrain.stdv40]')
+% xlabel('File')
+% saveas(gcf,strcat(directorys,'Stdev Near sec Peak','.pdf'))
+
+function [names] = createnamevec(structure)
+    names=string.empty;
+    for i = 1:length(structure)
+        names(i)=structure(i).name;
+    end
+    names=names';
+end
+
+function [structure] = plotcurves(structure, directorys)
+    fignumb =  length(findobj('type','figure'));
+    for i = 1:length(structure)
+        [~,name,~] = fileparts(structure(i).name);
+        figure(i+fignumb)
+        subplot(2,1,1)
+        plot(structure(i).time,structure(i).acc)
+        title(name)
+        xlabel('Time')
+        ylabel('Acc')
+        subplot(2,1,2)
+        plot(structure(i).freq,structure(i).mag,'DisplayName','FFT')
+        hold on
+        plot(structure(i).freqnear20, structure(i).magnear20, 'r.', 'LineWidth', 2, 'MarkerSize', 25, 'DisplayName','Max Near 20');
+        hold on
+        plot(structure(i).freqnear40, structure(i).magnear40, 'g.', 'LineWidth', 2, 'MarkerSize', 25,'DisplayName','Max Near 60');
+        legend;
+        xlabel('freq(Hz)')
+        xlim([0 100])
+        ylabel('Mag')
+        hold off
+        saveas(gcf,strcat(directorys,name,'.pdf'))
+    end
 end
 
 function [structure] = createstructure(directory,structure,HorF)
@@ -37,17 +98,21 @@ function [structure] = createstructure(directory,structure,HorF)
         [structure(i).time,structure(i).acc]=Opentxtfile(structure(i).name);
         [structure(i).freq,structure(i).mag]=fftfull(structure(i).acc);
         structure(i).HorF=HorF;
+        [structure(i).freqnear20,structure(i).magnear20] = findmax(structure(i).freq,structure(i).mag,20*0.8,20*1.2);
+        [structure(i).freqnear40,structure(i).magnear40] = findmax(structure(i).freq,structure(i).mag,40*0.8,40*1.2);
+        structure(i).stdv20=stdevr(structure(i).freq,structure(i).mag,structure(i).freqnear20);
+        structure(i).stdv40=stdevr(structure(i).freq,structure(i).mag,structure(i).freqnear40);
         j=j+1;
     end
 end
 
 function [freq,mag] = fftfull(acc)
-    L=length(acc);
+    lenacc=length(acc);
     Fs=2560;
-    NFFT = 2^nextpow2(L); % Next power of 2 from length of y
-    mag = fft(acc,NFFT)/L;
-    freq = (Fs/2*linspace(0,1,NFFT/2+1))';
-    mag=(2*abs(mag(1:NFFT/2+1)));
+    nexp = 2^nextpow2(lenacc);
+    mag = fft(acc,nexp)/lenacc;
+    freq = (Fs/2*linspace(0,1,nexp/2+1))';
+    mag=(2*abs(mag(1:nexp/2+1)));
 end
 
 function [time,acc] = Opentxtfile(file)
@@ -62,4 +127,17 @@ function [time,acc] = Opentxtfile(file)
     for i=2:length(time)
         time(i)=time(i-1)+(1/2560);
     end
+end
+
+function [freqmax,magmax] = findmax(freq,mag,mintarget,maxtarget)
+    [~,idmax]=min(abs(freq-maxtarget));
+    [~,idmin]=min(abs(freq-mintarget));
+    [magmax,idf]=max(mag(idmin:idmax));
+    freqmax=freq(idf+idmin);
+end
+
+function stdevf = stdevr(freq,mag,freqt)
+    [~,idmax]=min(abs(freq-(freqt*1.2)));
+    [~,idmin]=min(abs(freq-(freqt*0.8)));
+    stdevf=std(mag(idmin:idmax));
 end
